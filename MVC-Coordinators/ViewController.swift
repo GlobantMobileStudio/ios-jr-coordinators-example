8fdaa4af92076bee0b8822a54001f9d204064ad8
JosueQuinones <josue.quinones@globant.com>
2019-05-02 13:12:31 -0500

    chore: Add initial files and .gitignore from gitignore.io

---
+++ MVC-Coordinators/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC-Coordinators
+//
+//  Created by Josue Emanuel Quinones Rivera on 5/2/19.
+//  Copyright © 2019 Globant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
