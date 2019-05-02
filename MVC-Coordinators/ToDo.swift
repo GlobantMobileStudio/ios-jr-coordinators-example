f995d853972c1703a100014adc87811a1da39ae1
JosueQuinones <josue.quinones@globant.com>
2019-05-02 15:47:05 -0500

    refactor: Retrieve data in table view from model, add bar buttons for edit rows and add new to do items

---
+++ MVC-Coordinators/ToDo.swift
@@ -0,0 +1,23 @@
+//
+//  ToDo.swift
+//  MVC-Coordinators
+//
+//  Created by Josue Emanuel Quinones Rivera on 5/2/19.
+//  Copyright © 2019 Globant. All rights reserved.
+//
+
+import UIKit
+
+//MARK: To Do model
+struct ToDo {
+    //MARK: Attributes
+    var title: String
+    var isComplete: Bool
+    var dueDate: Date
+    var notes: String?
+    
+    //MARK: Methods for load data
+    static func loadToDos() -> [ToDo]? {
+        return nil
+    }
+}
--- MVC-Coordinators/ToDoTableViewController.swift
+++ MVC-Coordinators/ToDoTableViewController.swift
@@ -11,24 +11,40 @@ import UIKit
 class ToDoTableViewController: UITableViewController {
 
     //MARK: Attributes
+    var toDos = [ToDo]()
     
     //MARK: ToDoTableViewController methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let savedToDos = ToDo.loadToDos() {
+            toDos = savedToDos
+        }
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ToDoCell")
+        navigationItem.leftBarButtonItem = editButtonItem
     }
 }
 
 //MARK: ToDoTableViewController Data Source
 extension ToDoTableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return toDos.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell", for: indexPath)
-        cell.textLabel?.text = "Cell \(indexPath.row + 1)"
+        cell.textLabel?.text = toDos[indexPath.row].title
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            toDos.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
     }
 }
